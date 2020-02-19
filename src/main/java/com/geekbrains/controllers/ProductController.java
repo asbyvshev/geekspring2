7dc46beb4285f29de66a5184e9f678330812afc4
asbyvshev <barseto4ka@live.ru>
2020-02-19 17:00:05 +0300

    прописаны основные аннотации и связь с сервисом(блл)

--- src/main/java/com/geekbrains/controllers/ProductController.java
+++ src/main/java/com/geekbrains/controllers/ProductController.java
@@ -1,4 +1,17 @@
 package com.geekbrains.controllers;
 
+import com.geekbrains.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/product")
 public class ProductController {
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
 }
