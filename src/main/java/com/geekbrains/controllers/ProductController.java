dc0f7fc2b30ebde33d07f5dbce5dae3196bbfc91
asbyvshev <barseto4ka@live.ru>
2020-02-20 15:01:40 +0300

    добавлено - переход к форме нового продукта - отображение результата создания продукта

--- src/main/java/com/geekbrains/controllers/ProductController.java
+++ src/main/java/com/geekbrains/controllers/ProductController.java
@@ -1,8 +1,11 @@
 package com.geekbrains.controllers;
 
+import com.geekbrains.entites.Product;
 import com.geekbrains.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,5 +16,18 @@ public class ProductController {
     @Autowired
     public void setProductService(ProductService productService) {
         this.productService = productService;
+    }
+
+    @RequestMapping("/newProduct")
+    public String showSimpleForm(Model uiModel) {
+        Product product = new Product();
+        uiModel.addAttribute("product", product);
+        return "product-form";
+    }
+
+    @RequestMapping("/resultForm")
+    public String resultForm(Model uiModel, @ModelAttribute("product") Product product) {
+        productService.addProduct(product.getId(),product.getTitle(),product.getCost());
+        return "product-form-result";
     }
 }
